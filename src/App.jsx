073248195baa3f7a3983a5a0aad11ba3eda33186
Atenas Pérez <97369170+At3nas@users.noreply.github.com>
2024-01-03 17:27:35 -0300

    Add App.jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useFetch } from './useFetch';
+import './styles/App.css';
 
 export const App = () => {
   const { data, error } = useFetch();
 
   return (
-    <>
+    <div className="container">
       <h1>Pokedex</h1>
-      {data && console.log(data)}
-    </>
+      <main className="main-content">
+
+        <aside className="pokemon-sprite-container">
+
+        </aside>
+        <section className="pokemon-list-container">
+          <select size={10}>
+          </select>
+        </section>
+      </main>
+    </div>
   )
 }
